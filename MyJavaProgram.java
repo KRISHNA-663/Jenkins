1e43bac164be9c3089b8c224aa6bab3b107829db
KRISHNA <krish7cbe@gmail.com>
2023-11-02 21:07:25 +0530

    test

--- MyJavaProgram.java
+++ MyJavaProgram.java
@@ -1,23 +1,12 @@
 import java.io.*;
 public class MyJavaProgram {
     public static void main(String[] args) {
-        // Scanner s = new Scanner(System.in);
-        // int n=s.nextInt();
-        // float amt=0;
-        // if(n>100 && n<=200)
-        // {
-        //     amt+=(n-100)*1.5;
-        // }
-        // else if(n<200 && n<=500)
-        // {
-        //     amt+=200;
-        //     amt+=(n-200)*3;
-        // }
-        // else{
-        //     amt+=1730;
-        //     amt+=(n-500)*6.6;
-        // }
-        // System.out.println(amt);
-	    System.out.println("Helllooo");
+        int x = 10;
+        if(x%2==0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
 	}
 }
