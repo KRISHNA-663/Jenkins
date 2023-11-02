269078158a81952e4b8e40090dc03e00bbae3cae
KRISHNA <krish7cbe@gmail.com>
2023-11-02 20:59:45 +0530

    test

--- MyJavaProgram.java
+++ MyJavaProgram.java
@@ -1,22 +1,23 @@
 import java.io.*;
 public class MyJavaProgram {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
-        float amt=0;
-        if(n>100 && n<=200)
-        {
-            amt+=(n-100)*1.5;
-        }
-        else if(n<200 && n<=500)
-        {
-            amt+=200;
-            amt+=(n-200)*3;
-        }
-        else{
-            amt+=1730;
-            amt+=(n-500)*6.6;
-        }
-        System.out.println(amt);
+        // Scanner s = new Scanner(System.in);
+        // int n=s.nextInt();
+        // float amt=0;
+        // if(n>100 && n<=200)
+        // {
+        //     amt+=(n-100)*1.5;
+        // }
+        // else if(n<200 && n<=500)
+        // {
+        //     amt+=200;
+        //     amt+=(n-200)*3;
+        // }
+        // else{
+        //     amt+=1730;
+        //     amt+=(n-500)*6.6;
+        // }
+        // System.out.println(amt);
+	    System.out.println("Helllooo");
 	}
 }
